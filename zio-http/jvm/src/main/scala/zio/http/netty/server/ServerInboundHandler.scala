a8431f625df3c00ac9aa1f9f5571934b85e90ab4
Saturn225 <101260782+Saturn225@users.noreply.github.com>
2025-01-23 15:05:13 +0530

    Update ServerInboundHandler.scala

--- zio-http/jvm/src/main/scala/zio/http/netty/server/ServerInboundHandler.scala
+++ zio-http/jvm/src/main/scala/zio/http/netty/server/ServerInboundHandler.scala
@@ -119,19 +119,14 @@ private[zio] final case class ServerInboundHandler(
     if (host != null) {
       val parts       = host.split(":")
       val isValidHost = parts(0).forall(c => c.isLetterOrDigit || c == '.' || c == '-')
-      val isValidPort = parts.length == 1 || (parts.length == 2 && parts(1).forall(_.isDigit))
-      val isValid     = isValidHost && isValidPort
-      if (!isValid) {
+      if (!isValidHost) {
         ZIO
           .logWarning(
             s"Invalid Host header for request ${req.method} ${req.url}. " +
-              s"Host: $host, isValidHost: $isValidHost, isValidPort: $isValidPort",
+              s"Host: $host, isValidHost: $isValidHost",
           )
       }
-      isValid
-    } else {
-      ZIO.logWarning(s"Missing Host header for request ${req.method} ${req.url}")
-      false
+      isValidHost
     }
   }
 
