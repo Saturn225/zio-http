f462dc626326606245eeb070610607b515527bed
Saturn225 <101260782+Saturn225@users.noreply.github.com>
2025-05-24 17:50:16 +0530

    Update ConformanceE2ESpec.scala

--- zio-http/jvm/src/test/scala/zio/http/ConformanceE2ESpec.scala
+++ zio-http/jvm/src/test/scala/zio/http/ConformanceE2ESpec.scala
@@ -7,17 +7,19 @@ import zio.http._
 import zio.http.internal.{DynamicServer, RoutesRunnableSpec}
 import zio.http.netty.NettyConfig
 object ConformanceE2ESpec extends RoutesRunnableSpec {
-  private val port             = 8080
-  private val MaxSize          = 1024 * 10
-  val configApp: Server.Config =
-    Server.Config.default
-      .requestDecompression(true)
-      .disableRequestStreaming(MaxSize)
-      .port(port)
-      .responseCompression()
-      .validateHeaders(true)
-  private val app              = serve
-  def conformanceSpec          = suite("ConformanceE2ESpec")(
+  private val port    = 8080
+  private val MaxSize = 1024 * 10
+  val baseConfig      = Server.Config.default
+    .requestDecompression(true)
+    .disableRequestStreaming(MaxSize)
+    .port(port)
+    .responseCompression()
+    .validateHeaders(true)
+
+  val runtimeConfig = baseConfig.validateHeaders(true)
+
+  private val app     = serve
+  def conformanceSpec = suite("ConformanceE2ESpec")(
     test("should return 400 Bad Request if Host header is missing") {
       val routes = Handler.ok.toRoutes
       val res    = routes.deploy.status.run(path = Path.root, headers = Headers(Header.Host("%%%%invalid%%%%")))
@@ -29,14 +31,14 @@ object ConformanceE2ESpec extends RoutesRunnableSpec {
       assertZIO(res)(equalTo(Status.Ok))
     },
   )
-  override def spec            =
+  override def spec   =
     suite("ConformanceE2ESpec") {
       val spec = conformanceSpec
       suite("app without request streaming") { app.as(List(spec)) }
     }.provideShared(
       Scope.default,
       DynamicServer.live,
-      ZLayer.succeed(configApp),
+      ZLayer.succeed(runtimeConfig),
       Server.customized,
       Client.default,
       ZLayer.succeed(NettyConfig.default),
