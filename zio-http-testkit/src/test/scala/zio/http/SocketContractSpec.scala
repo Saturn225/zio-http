6ec4f9557bc7d291f4a400cf7de2d14264404bfb
Saturn225 <101260782+Saturn225@users.noreply.github.com>
2025-05-25 14:26:12 +0530

    Update SocketContractSpec.scala

--- zio-http-testkit/src/test/scala/zio/http/SocketContractSpec.scala
+++ zio-http-testkit/src/test/scala/zio/http/SocketContractSpec.scala
@@ -1,145 +1,155 @@
-package zio.http
+/*
+ * Copyright 2021 - 2023 Sporta Technologies PVT LTD & the ZIO HTTP contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import zio.Console.printLine
+package zio.http.netty.server
+
+import java.lang.{Boolean => JBoolean}
+import java.net.InetSocketAddress
+
 import zio._
-import zio.test.Assertion._
-import zio.test._
 
-import zio.http.ChannelEvent.{Read, Unregistered, UserEvent, UserEventTriggered}
-import zio.http.netty.NettyConfig
-import zio.http.netty.server.NettyDriver
+import zio.http.Driver.StartResult
+import zio.http._
+import zio.http.netty._
+import zio.http.netty.client.NettyClientDriver
 
-object SocketContractSpec extends ZIOHttpSpec {
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.{Channel => NettyChannel, ChannelFactory, ChannelInitializer, ChannelOption, ServerChannel}
+import io.netty.util.ResourceLeakDetector
 
-  def spec: Spec[Any, Any] =
-    suite("SocketOps")(
-      contract("Successful Multi-message application") { p =>
-        val socketServer: WebSocketApp[Any] =
-          Handler.webSocket { channel =>
-            channel.receiveAll {
-              case Read(WebSocketFrame.Text("Hi Server"))          =>
-                printLine("Server got message: Hi Server") *> channel.shutdown
-              case UserEventTriggered(UserEvent.HandshakeComplete) =>
-                channel.send(ChannelEvent.Read(WebSocketFrame.text("Hi Client")))
-              case Unregistered                                    =>
-                p.succeed(()) *>
-                  printLine("Server Channel unregistered")
-              case other                                           =>
-                printLine("Server Unexpected: " + other)
-            }
-          }
+private[zio] final case class NettyDriver(
+  appRef: RoutesRef,
+  channelFactory: ChannelFactory[ServerChannel],
+  channelInitializer: ChannelInitializer[NettyChannel],
+  serverInboundHandler: ServerInboundHandler,
+  eventLoopGroups: ServerEventLoopGroups,
+  serverConfig: ServerRuntimeConfig,
+  nettyConfig: NettyConfig,
+) extends Driver { self =>
 
-        socketServer
-      } { _ =>
-        val socketClient: WebSocketApp[Any] =
-          Handler.webSocket { channel =>
-            channel.receiveAll {
-              case ChannelEvent.Read(WebSocketFrame.Text("Hi Client")) =>
-                channel.send(Read(WebSocketFrame.text("Hi Server")))
-              case ChannelEvent.Unregistered                           =>
-                printLine("Client Channel unregistered")
-              case other                                               =>
-                printLine("Client received Unexpected event: " + other)
-            }
-          }
+  def start(implicit trace: Trace): RIO[Scope, StartResult] =
+    for {
+      chf     <- ZIO.attempt {
+        new ServerBootstrap()
+          .group(eventLoopGroups.boss, eventLoopGroups.worker)
+          .channelFactory(channelFactory)
+          .childHandler(channelInitializer)
+          .option[Integer](ChannelOption.SO_BACKLOG, serverConfig.config.soBacklog)
+          .childOption[JBoolean](ChannelOption.TCP_NODELAY, serverConfig.config.tcpNoDelay)
+          .bind(serverConfig.config.address)
+      }
+      _       <- NettyFutureExecutor.scoped(chf)
+      _       <- ZIO.succeed(ResourceLeakDetector.setLevel(nettyConfig.leakDetectionLevel.toNetty))
+      channel <- ZIO.attempt(chf.channel())
+      port    <- ZIO.attempt(channel.localAddress().asInstanceOf[InetSocketAddress].getPort)
 
-        socketClient
-      },
-      contract("Application where server app fails")(_ =>
-        Handler.webSocket { channel =>
-          channel.receiveAll {
-            case UserEventTriggered(UserEvent.HandshakeComplete) =>
-              ZIO.fail(new Exception("Broken server"))
-            case _                                               =>
-              ZIO.unit
-          }.ensuring(channel.shutdown)
-        },
-      ) { p =>
-        Handler.webSocket { channel =>
-          channel.receiveAll {
-            case Unregistered =>
-              printLine("Server failed and killed socket. Should complete promise.") *>
-                p.succeed(()).unit
-            case _            =>
-              ZIO.unit
-          }
-        }
-      },
-      contract("Application where client app fails")(p =>
-        Handler.webSocket { channel =>
-          channel.receiveAll {
-            case Unregistered =>
-              printLine("Client failed and killed socket. Should complete promise.") *>
-                p.succeed(()).unit
-            case _            =>
-              ZIO.unit
-          }
-        },
-      ) { _ =>
-        Handler.webSocket { channel =>
-          channel.receiveAll {
-            case UserEventTriggered(UserEvent.HandshakeComplete) =>
-              ZIO.fail(new Exception("Broken client"))
-            case _                                               => ZIO.unit
-          }.ensuring(channel.shutdown)
-        }
-      },
+      _ <- Scope.addFinalizer(
+        NettyFutureExecutor.executed(channel.close()).ignoreLogged,
+      )
+    } yield StartResult(port, serverInboundHandler.inFlightRequests)
+
+  def addApp[R](newApp: Routes[R, Response], env: ZEnvironment[R])(implicit trace: Trace): UIO[Unit] =
+    ZIO.fiberId.map { fiberId =>
+      var loop = true
+      while (loop) {
+        val oldAppAndRt     = appRef.get()
+        val (oldApp, oldRt) = oldAppAndRt
+        val updatedApp      = (oldApp ++ newApp).asInstanceOf[Routes[Any, Response]]
+        val updatedEnv      = oldRt.environment.unionAll(env)
+        // Update the fiberRefs with the new environment to avoid doing this every time we run / fork a fiber
+        val updatedFibRefs  = oldRt.fiberRefs.updatedAs(fiberId)(FiberRef.currentEnvironment, updatedEnv)
+        val updatedRt       = Runtime(updatedEnv, updatedFibRefs, oldRt.runtimeFlags)
+        val updatedAppAndRt = (updatedApp, updatedRt)
+
+        if (appRef.compareAndSet(oldAppAndRt, updatedAppAndRt)) loop = false
+      }
+      serverInboundHandler.refreshApp()
+    }
+
+  override def createClientDriver()(implicit trace: Trace): ZIO[Scope, Throwable, ClientDriver] =
+    for {
+      channelFactory <- ChannelFactories.Client.live.build
+        .provideSomeEnvironment[Scope](_ ++ ZEnvironment[ChannelType.Config](nettyConfig))
+      nettyRuntime   <- NettyRuntime.live.build
+    } yield NettyClientDriver(channelFactory.get, eventLoopGroups.worker, nettyRuntime.get)
+
+  override def toString: String = s"NettyDriver($serverConfig)"
+}
+
+object NettyDriver {
+
+  implicit val trace: Trace = Trace.empty
+
+  val make: ZIO[
+    RoutesRef
+      & ChannelFactory[ServerChannel]
+      & ChannelInitializer[NettyChannel]
+      & ServerEventLoopGroups
+      & ServerRuntimeConfig
+      & NettyConfig
+      & ServerInboundHandler,
+    Nothing,
+    Driver,
+  ] =
+    for {
+      app   <- ZIO.service[RoutesRef]
+      cf    <- ZIO.service[ChannelFactory[ServerChannel]]
+      cInit <- ZIO.service[ChannelInitializer[NettyChannel]]
+      elg   <- ZIO.service[ServerEventLoopGroups]
+      sc    <- ZIO.service[ServerRuntimeConfig]
+      nsc   <- ZIO.service[NettyConfig]
+      sih   <- ZIO.service[ServerInboundHandler]
+    } yield new NettyDriver(
+      appRef = app,
+      channelFactory = cf,
+      channelInitializer = cInit,
+      serverInboundHandler = sih,
+      eventLoopGroups = elg,
+      serverConfig = sc,
+      nettyConfig = nsc,
     )
 
-  private def contract(
-    name: String,
-  )(
-    serverApp: Promise[Throwable, Unit] => WebSocketApp[Any],
-  )(clientApp: Promise[Throwable, Unit] => WebSocketApp[Any]) = {
-    suite(name)(
-      test("Live") {
-        for {
-          portAndPromise <- liveServerSetup(serverApp)
-          (port, promise) = portAndPromise
-          url      <- ZIO.fromEither(URL.decode(s"ws://localhost:$port/")).orDie
-          response <- ZIO.serviceWithZIO[Client](
-            _.driver.socket(Version.Http_1_1, url, Headers.empty, clientApp(promise)),
-          )
-          _        <- promise.await.timeout(10.seconds)
-        } yield assert(response.status)(equalTo(Status.SwitchingProtocols))
-      }.provideSome[Client](
-        TestServer.layer,
-        ZLayer.fromFunction(ServerRuntimeConfig.apply) >>> ZLayer.succeed(Server.Config.default.onAnyOpenPort),
-        NettyDriver.customized,
-        ZLayer.succeed(NettyConfig.defaultWithFastShutdown),
-        Scope.default,
-      ).provide(Client.default),
-      test("Test") {
-        for {
-          portAndPromise <- testServerSetup(serverApp)
-          (port, promise) = portAndPromise
-          url      <- ZIO.fromEither(URL.decode(s"ws://localhost:$port/")).orDie
-          response <- ZIO.serviceWithZIO[Client](
-            _.driver.socket(Version.Http_1_1, url, Headers.empty, clientApp(promise)),
-          )
-          _        <- promise.await.timeout(10.seconds)
-        } yield assert(response.status)(equalTo(Status.SwitchingProtocols))
-      }.provide(TestClient.layer, Scope.default),
+  val manual: ZLayer[ServerEventLoopGroups & ChannelFactory[
+    ServerChannel,
+  ] & ServerRuntimeConfig & NettyConfig, Nothing, Driver] = {
+    implicit val trace: Trace = Trace.empty
+    ZLayer.makeSome[ServerEventLoopGroups & ChannelFactory[ServerChannel] & ServerRuntimeConfig & NettyConfig, Driver](
+      ZLayer(AppRef.empty),
+      ServerChannelInitializer.layer,
+      ServerInboundHandler.live,
+      ZLayer(make),
     )
   }
 
-  private def liveServerSetup(
-    serverApp: Promise[Throwable, Unit] => WebSocketApp[Any],
-  ): ZIO[Server, Nothing, (RuntimeFlags, Promise[Throwable, Unit])] =
-    ZIO.serviceWithZIO[Server](server =>
-      for {
-        p    <- Promise.make[Throwable, Unit]
-        _    <- server.installInternal(serverApp(p).toRoutes)
-        port <- server.port
-      } yield (port, p),
+  val customized: ZLayer[ServerRuntimeConfig & NettyConfig, Throwable, Driver] = {
+    val serverChannelFactory: ZLayer[NettyConfig, Nothing, ChannelFactory[ServerChannel]] =
+      ChannelFactories.Server.fromConfig
+    val eventLoopGroup: ZLayer[NettyConfig, Nothing, ServerEventLoopGroups]               = ServerEventLoopGroups.live
+
+    ZLayer.makeSome[ServerRuntimeConfig & NettyConfig, Driver](
+      eventLoopGroup,
+      serverChannelFactory,
+      manual,
     )
+  }
 
-  private def testServerSetup(
-    serverApp: Promise[Throwable, Unit] => WebSocketApp[Any],
-  ): ZIO[TestClient, Nothing, (RuntimeFlags, Promise[Throwable, Unit])] =
-    for {
-      p <- Promise.make[Throwable, Unit]
-      _ <- TestClient.installSocketApp(serverApp(p))
-    } yield (0, p)
-
+  val live: ZLayer[Server.Config, Throwable, Driver] =
+    ZLayer.makeSome[Server.Config, Driver](
+      ZLayer.succeed(NettyConfig.default),
+      ServerRuntimeConfig.layer,
+      customized,
+    )
 }
