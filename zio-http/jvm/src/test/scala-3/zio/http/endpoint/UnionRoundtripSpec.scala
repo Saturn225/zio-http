16390496f5d57f531957479b3ff3f0db3cd2d4da
Saturn225 <101260782+Saturn225@users.noreply.github.com>
2025-05-25 14:52:24 +0530

    Update UnionRoundtripSpec.scala

--- zio-http/jvm/src/test/scala-3/zio/http/endpoint/UnionRoundtripSpec.scala
+++ zio-http/jvm/src/test/scala-3/zio/http/endpoint/UnionRoundtripSpec.scala
@@ -39,6 +39,7 @@ import zio.http.netty.NettyConfig
 object UnionRoundtripSpec extends ZIOHttpSpec {
   val testLayer: ZLayer[Any, Throwable, Server & Client & Scope] =
     ZLayer.make[Server & Client & Scope](
+      ServerRuntimeConfig.layer
       Server.customized,
       ZLayer.succeed(Server.Config.default.onAnyOpenPort.enableRequestStreaming),
       Client.customized.map(env => ZEnvironment(env.get)),
