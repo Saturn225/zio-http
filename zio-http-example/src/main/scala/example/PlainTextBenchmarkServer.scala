97be4a8a10cfbe4a3a3e43e09d9dfcf1138430c7
Saturn225 <101260782+Saturn225@users.noreply.github.com>
2025-10-25 15:52:14 +0530

    Remove unnecessary server runtime config layer from benchmark servers

--- zio-http-example/src/main/scala/example/PlainTextBenchmarkServer.scala
+++ zio-http-example/src/main/scala/example/PlainTextBenchmarkServer.scala
@@ -44,9 +44,8 @@ object PlainTextBenchmarkServer extends ZIOAppDefault {
 
   private val configLayer              = ZLayer.succeed(config)
   private val nettyConfigLayer         = ZLayer.succeed(nettyConfig)
-  private val serverRuntimeConfigLayer = configLayer.flatMap(env => ZLayer.succeed(Config(env.get)))
 
   override val run: ZIO[Environment with ZIOAppArgs with Scope, Any, Any] =
-    Server.serve(routes).provide(serverRuntimeConfigLayer, nettyConfigLayer, Server.customized)
+    Server.serve(routes).provide(configLayer, nettyConfigLayer, Server.customized)
 
 }
--- zio-http-example/src/main/scala/example/SimpleEffectBenchmarkServer.scala
+++ zio-http-example/src/main/scala/example/SimpleEffectBenchmarkServer.scala
@@ -41,9 +41,8 @@ object SimpleEffectBenchmarkServer extends ZIOAppDefault {
 
   private val configLayer              = ZLayer.succeed(config)
   private val nettyConfigLayer         = ZLayer.succeed(nettyConfig)
-  private val serverRuntimeConfigLayer = configLayer.flatMap(env => ZLayer.succeed(Config(env.get)))
 
   override val run =
-    Server.serve(routes).provide(serverRuntimeConfigLayer, nettyConfigLayer, Server.customized)
+    Server.serve(routes).provide(configLayer, nettyConfigLayer, Server.customized)
 
 }
